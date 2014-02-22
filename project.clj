419d2c9ceb387df674e0491b2f759d0149d17c47
ka <ka.kaosf@gmail.com>
2014-02-23 01:47:24 +0900

    modify project.clj for lein-cljsbuild
    ref. https://github.com/emezeske/lein-cljsbuild#basic-configuration

--- project.clj
+++ project.clj
@@ -3,4 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
+  :cljsbuild {
+    :builds [{
+      ; The path to the top-level ClojureScript source directory:
+      :source-paths ["src-cljs"]
+      ; The standard ClojureScript compiler options:
+      ; (See the ClojureScript compiler documentation for details.)
+      :compiler {
+        :output-to "war/javascripts/main.js"  ; default: target/cljsbuild-main.js
+        :optimizations :whitespace
+        :pretty-print true}}]})
